d65f8f250685c76e0c8b6835065b9baeec230684
Uma jaiswal <umaj2631@gmail.com>
2026-09-13 18:51:28 +0530

    Initial commit

---
+++ Netflix/script.js
@@ -0,0 +1,92 @@
+// alert("hello world")
+
+// console.log("Heloo world")
+// console.log("code is also running...")
+// console.log("code is looking like a wow...")
+
+// var a = prompt("Enter your number")
+// var isTrue = confirm("Are you sure you want to leave this page and blast this computer")
+
+// if(isTrue){
+//     console.log("computer is blasting")
+// }
+// else{
+//     console.log("computer is blasting..")
+// }
+// console.log("your number is " + a)
+// console.log("Hey this is tutorial 55")
+
+// var a=7;
+// var b=7;
+// var _a = "Shubham"
+
+// console.log(a+b+8)
+// console.log(typeof a, typeof b,typeof c)
+// let x= "harry bhaiii";
+// let y= 22;
+// let z= 3.55;
+// const p= true;
+// let q= undefined;
+// let r= null;
+
+// console.log(x,y,z,p,q,r)
+// console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+// let o={
+//     "name": "harry",
+//     "job role": 5644
+// }
+// console.log(o)
+// o.salary = "100crores"
+// console.log(o)
+// console.log("hello i am conditional tutorial")
+
+// let age = 18;
+// // let grace =2;
+// if(age==18){
+// console.log("you can drive");
+// }
+// else{
+//     console.log("you cannot drive");
+// }
+//console.log("I am a tutorial on loop")
+//let a=1;
+// console.log(A)
+// console.log(A+1)
+// console.log(A+2)
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+    
+// }
+// let obj ={
+//     name:"harry",
+//     role: "programmer",
+//     company: "codewithharry ai"
+// }
+
+//    for(const key in obj) {
+//     console.log(key)
+// }
+
+// for(const c of  "harry"){
+// console.log(c)
+// }
+// let i = 0;
+// while (i<5) {
+//     console.log(i)
+//     i++;
+// }
+// function nice(name) {
+//     console.log( "hey " + name + " you are nice!")
+//     console.log( "hey "+ name + " you are nice!!")
+//     console.log( "hey "+ name + " you are nice!!!")
+//     console.log( "hey "+ name + " you are nice!!!!")
+// }
+// nice("Vertika")
+// function sum(a, b) {
+//     console.log(a + b)
+// }
+// sum(3, 5)
+// console.log("Harshita")
+//  let Boxes= document.getElementsByClassName ("box")
+//  console.log(Bhupendrajogi)
+//  Boxes[2].style.backgroundcolor = "red"
